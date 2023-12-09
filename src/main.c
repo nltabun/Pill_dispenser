e762cc8d882a0eb6eb1ec79af57877dd7d9ff56d
rsth <nltabun@outlook.com>
2023-12-09 15:52:16 +0200

    init stuff

--- src/main.c
+++ src/main.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_0 20
+#define LED_1 21
+#define LED_2 22
+#define BUTTON_SW0 9
+#define BUTTON_SW1 8
+#define BUTTON_SW2 7
+#define UART_NR 1 
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+#define UART_BAUD_RATE 9600
+#define I2C_PORT i2c1
+#define I2C_PORT_SDA_PIN 14
+#define I2C_PORT_SDL_PIN 15
+#define I2C_BAUD_RATE 100000
+#define STEPPER_PIN_A 2
+#define STEPPER_PIN_B 3
+#define STEPPER_PIN_C 6
+#define STEPPER_PIN_D 13
+#define OPTO_FORK_PIN 28
+
+enum State {
+    WAIT = 0,
+    CALIBRATE = 1,
+    READY = 2,
+    DISPENSING = 3
+} DispenserState;
+
 int main() {
+    init_all();
 
-    const uint led_pin = 22;
-    uint count = 0;
-
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-
-    // Initialize chosen serial port
-    stdio_init_all();
-
-    // Loop forever
     while (true) {
 
-        // Blink LED
-        printf("Blinking! %u\r\n", ++count);
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
-        sleep_ms(1000);
     }
+}
+
+void init_all()
+{
+    stdio_init_all();
+
+    gpio_init(STEPPER_PIN_A);
+    gpio_init(STEPPER_PIN_B);
+    gpio_init(STEPPER_PIN_C);
+    gpio_init(STEPPER_PIN_D);
+    gpio_init(OPTO_FORK_PIN);
+    gpio_set_dir(STEPPER_PIN_A, GPIO_OUT);
+    gpio_set_dir(STEPPER_PIN_B, GPIO_OUT);
+    gpio_set_dir(STEPPER_PIN_C, GPIO_OUT);
+    gpio_set_dir(STEPPER_PIN_D, GPIO_OUT);
+    gpio_set_dir(OPTO_FORK_PIN, GPIO_IN);
+    gpio_pull_up(OPTO_FORK_PIN);
 }
