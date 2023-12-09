6702f9099f7858768febf8a165843de34964a149
rsth <nltabun@outlook.com>
2023-12-09 17:52:37 +0200

    state_machine ea, init all

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
 #include "../src/stepper_motor.h"
 
 #define LED_0 20
@@ -8,7 +10,7 @@
 #define BUTTON_SW0 9
 #define BUTTON_SW1 8
 #define BUTTON_SW2 7
-#define UART_NR 1 
+#define UART_NR 1
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 #define UART_BAUD_RATE 9600
@@ -17,27 +19,92 @@
 #define I2C_PORT_SDL_PIN 15
 #define I2C_BAUD_RATE 100000
 
-
-enum State {
+enum DispenserState
+{
     WAIT = 0,
     CALIBRATE = 1,
     READY = 2,
     DISPENSING = 3
-} DispenserState;
+};
 
 void init_all();
 
-int main() {
+int main()
+{
+    enum DispenserState state;
+    bool led0 = false;
+    u_int8_t led_timer = 0;
+
+
     init_all();
 
-    while (true) {
+    state = WAIT;
 
+    while (true)
+    {
+        switch (state)
+        {
+        case WAIT:
+            while (gpio_get(BUTTON_SW1))
+            {
+                if (led_timer > 100)
+                {
+                    led0 = !led0;
+                    gpio_put(LED_0, led0);
+                    led_timer = 0;
+                }
+                led_timer++;
+                sleep_ms(10);
+            }
+            gpio_put(LED_0, (led0 = false));
+
+            while (!gpio_get(BUTTON_SW1))
+                sleep_ms(10);
+            sleep_ms(100);
+            state = CALIBRATE;
+            break;
+        case CALIBRATE:
+            //calibrate(&motor_steps, 3);
+            state = READY;
+            break;
+        case READY:
+            state = DISPENSING;
+            /* code */
+            break;
+        case DISPENSING:
+            /* code */
+            state = WAIT;
+            break;
+        default:
+            state = WAIT;
+            break;
+        }
     }
 }
 
 void init_all()
 {
     stdio_init_all();
+
+    gpio_init(LED_0);
+    gpio_init(LED_1);
+    gpio_init(LED_2);
+
+    gpio_set_dir(LED_0, GPIO_OUT);
+    gpio_set_dir(LED_1, GPIO_OUT);
+    gpio_set_dir(LED_2, GPIO_OUT);
+
+    gpio_init(BUTTON_SW0);
+    gpio_init(BUTTON_SW1);
+    gpio_init(BUTTON_SW2);
+
+    gpio_set_dir(BUTTON_SW0, GPIO_IN);
+    gpio_set_dir(BUTTON_SW1, GPIO_IN);
+    gpio_set_dir(BUTTON_SW2, GPIO_IN);
+
+    gpio_pull_up(BUTTON_SW0);
+    gpio_pull_up(BUTTON_SW1);
+    gpio_pull_up(BUTTON_SW2);
 
     gpio_init(STEPPER_PIN_A);
     gpio_init(STEPPER_PIN_B);
